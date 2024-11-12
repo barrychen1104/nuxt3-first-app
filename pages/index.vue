c27309963c7f5d2d6dab43ee21ce5825a37dbe5a
BarryChen <s481838@sinyi.com.tw>
2024-11-12 11:49:40 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div>
+   <h1>Page: 首頁</h1>
+   <p>目前路由的路徑 - route.fullPath為:{{ route.fullPath }} </p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/product/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    Page: product/{{ route.params.id }}
+  </div>
+</template>
+
+<style scoped></style>
